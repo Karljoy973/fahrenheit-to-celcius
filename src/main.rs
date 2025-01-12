f3c46efb5700845d85a4e0ab33c226523e2c037f
973Karljoy <karljoyeux@gmail.com>
2025-01-12 04:16:40 +0100

    Finished

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+
+use std::io;
+fn main() {
+
+/// fahrencius 
+/// Goal : Convert Fahrenheit temperatures in Celcius temperatures 
+/// 
+    fn fahrencius(t: f32) -> f32{
+       return  (t-32.0)*5./9.; 
+    }
+    println!("Which temperature is it in Fahrenheit ? ");
+    let mut temperature = String::new();
+    io::stdin().read_line(&mut temperature).expect("Not nice");
+    let temperature:f32 = match temperature.trim().parse() {
+        Ok(value) => value,
+        Err(_) => -293.,
+    };
+    println!("You entered {temperature} F");
+    let temperature_in_celcius = fahrencius(temperature);
+
+    println!("The corresponding temperature in degrees Celcius is: {temperature_in_celcius} °C");
+
+
+}
