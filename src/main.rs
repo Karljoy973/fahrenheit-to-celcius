216d437e714aa0763760a5e22743c52d61511c12
973Karljoy <karljoyeux@gmail.com>
2025-01-12 04:34:23 +0100

    Erata, adding a conditional checking on the input value

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,21 @@ fn main() {
     }
     println!("Which temperature is it in Fahrenheit ? ");
     let mut temperature = String::new();
+    println!("You entered {temperature} F");
     io::stdin().read_line(&mut temperature).expect("Not nice");
     let temperature:f32 = match temperature.trim().parse() {
         Ok(value) => value,
-        Err(_) => -293.,
+        Err(_) => -800.,
     };
-    println!("You entered {temperature} F");
     let temperature_in_celcius = fahrencius(temperature);
 
-    println!("The corresponding temperature in degrees Celcius is: {temperature_in_celcius} °C");
+    if temperature_in_celcius == fahrencius(-800.){
+        println!("There was probably an error when entering the temperature but here is the default Celcius value: {temperature_in_celcius} °C")
+    }
+
+    else {
+        println!("The corresponding temperature in degrees Celcius is: {temperature_in_celcius} °C");
+    }
 
 
 }
